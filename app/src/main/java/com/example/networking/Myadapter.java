43755e58d4d8b2c73cd22fbcf6acab3f27e8e596
b21emmbj <b21emmbj@student.his.se>
2022-04-27 16:17:02 +0200

    skapade en ny javaklass för viewholder, la till alla nödvändiga metoder i adaptern och i viewholdern

--- app/src/main/java/com/example/networking/Myadapter.java
+++ app/src/main/java/com/example/networking/Myadapter.java
@@ -1,4 +1,24 @@
 package com.example.networking;
 
-public class Myadapter {
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Myadapter extends RecyclerView.Adapter<Viewholder>{
+    @NonNull
+    @Override
+    public Viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Viewholder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
---
+++ app/src/main/java/com/example/networking/Viewholder.java
@@ -0,0 +1,12 @@
+package com.example.networking;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Viewholder extends RecyclerView.ViewHolder {
+    public Viewholder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
