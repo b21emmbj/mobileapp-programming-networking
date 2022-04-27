045e274295d734302c0473b532e2452e32fb62f6
b21emmbj <b21emmbj@student.his.se>
2022-04-27 16:45:03 +0200

    skapade en adapterlayoutitem

--- app/src/main/java/com/example/networking/Myadapter.java
+++ app/src/main/java/com/example/networking/Myadapter.java
@@ -3,9 +3,14 @@ package com.example.networking;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class Myadapter extends RecyclerView.Adapter<Viewholder>{
+
+    private List<Mountain> mountains;
     @NonNull
     @Override
     public Viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
